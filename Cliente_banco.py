4da22c00a9bc1cb249bb285b4a2e4ba1bcd1a562
Felipe Nascimento <r.nascimento.felipe@gmail.com>
2022-12-14 12:47:50 -0300

    Solucionado problema de login no início do programa, e adicionado comentários

--- Cliente_banco.py
+++ Cliente_banco.py
@@ -1,28 +1,32 @@
 import socket
 
-
+# Função que administra a conexão no lado do cliente,
+# possibilita a troca de mensagens com o seridor
 def cliente_banco():
     host = socket.gethostname()  # Recebe o nome do host, que também é usado pelo servidor por estarem ambos na mesma máquina
-    port = 5001  #  Número da porta do servidor socket
+    port = 5001  # Número da porta do servidor socket
 
     client_socket = socket.socket()  # Inicia a instância do socket
     client_socket.connect((host, port))  # Conecta ao servidor
 
-    message = 'iniciar'  # aguarda a entrada de dados
+    mensagem = ''  # Define a mensagem
 
-    while message.lower().strip() != 'finalizar': #se digitar finalizar, termina o programa para o cliente e o servidor tb
-        client_socket.send(message.encode())  # envia a mensagem para o servidor
-        data = client_socket.recv(1024).decode()  # recebe a resposta do servidor
+    # Se digitar "finalizar", termina o programa para o cliente e o servidor
+    while mensagem.lower().strip() != 'finalizar':
+        data = client_socket.recv(1024).decode()  # Recebe a resposta do servidor
 
-        print('Recebido do servidor: ' + data)  # Mostra os dados recebidos no terminal
+        # Mostra os dados recebidos do servidor no terminal
+        print('Recebido do servidor: ' + data)
 
+        # Se receber do servidor a resposta de encerramento da operação, finaliza o programa
         if data == 'Encerrando...':
             client_socket.close()
             exit()
 
-        message = input("")  # aguarda uma nova entrada de dados do usuario
+        mensagem = input("")  # Aguarda uma nova entrada de dados do usuario
+        client_socket.send(mensagem.encode())  # Envia a mensagem para o servidor
 
-    client_socket.close()  # finaliza a conexão
+    client_socket.close()  # Finaliza a conexão
 
 
 if __name__ == '__main__':
